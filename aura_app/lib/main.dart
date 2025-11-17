e67c877cff30a6a41e053ac3931c225efb13385b
nibin-joseph05 <nibin.joseph.career@gmail.com>
2025-11-17 14:00:14 +0530

    renamed features -> screens

--- aura_app/lib/main.dart
+++ aura_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:aura_app/features/splash/splash_screen.dart';
+import 'package:aura_app/screens/splash/splash_screen.dart';
 
 void main() {
   runApp(const AuraApp());
